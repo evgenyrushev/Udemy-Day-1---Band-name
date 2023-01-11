c96f5cb33bd35be47ff8c6d3a51c26c58ae9bc0f
evgenyrushev <122179434+evgenyrushev@users.noreply.github.com>
2023-01-11 22:59:35 +0200

    Add files via upload

---
+++ Music Band.py
@@ -0,0 +1,8 @@
+print("What is the name of the city you were born in?")
+answer_city = str(input())
+
+print("What is the name of your pet?")
+answer_pet = str(input())
+
+print(f"Your band name shoud be called {answer_city} {answer_pet}!")
+
